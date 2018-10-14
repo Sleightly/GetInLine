b24841f0383fa49fe31f90db21954ba5ec7cfa78
Sleightly <ejc042@ucsd.edu>
2018-10-14 05:43:12 -0700

    final changes

--- data_parser.py
+++ data_parser.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np 
 
-df = pd.read_csv('data/data_line1.csv')
+df = pd.read_csv('data/data_line2.csv')
 
 vel = []
 y = []
--- obj_detect.py
+++ obj_detect.py
@@ -90,7 +90,7 @@ def resize_imgs(count, name):
 def cross_correlation(count, name):
     #split resize image into fourths
     magnitude = []
-    for i in range(1):
+    for i in range(count-1):
         path = "resize/{}/res{}.jpg".format(name, i)
         f_path = "resize/{}/res{}.jpg".format(name, i+1)
 
@@ -111,11 +111,6 @@ def cross_correlation(count, name):
             indices.append(start_col)
             start_row, start_col = 0, end_col
             end_row, end_col = height, (end_col+width_offset)
-
-
-        for i in range(20):
-            testpath = "test/{}.jpg".format(name, i)
-            cv2.imwrite(testpath, cropped[i])
         
         #compare to future image
         f_image = cv2.imread(f_path)
@@ -164,18 +159,18 @@ def cross_correlation(count, name):
 
 if __name__ == '__main__':
     name = 'line2'
-    #count = count_imgs('videos/'+name+'.mp4', name)
+    count = count_imgs('videos/'+name+'.mp4', name)
     #count = 302
     #print("resizing")
-    #resize_imgs(count, name)
-    #print("cross correlation")
+    resize_imgs(count, name)
+    print("cross correlation")
     magnitude = cross_correlation(count, name)
-    #print('drawing boxes')
-    #num_people = localize_objects(count, name, magnitude)
+    print('drawing boxes')
+    num_people = localize_objects(count, name, magnitude)
     #print("printing photos")
     #print_photos(count, name)
-    #df = pd.DataFrame({'magnitude':magnitude, 'people':num_people})
-    #df.to_csv("data/data_"+name+".csv")
+    df = pd.DataFrame({'magnitude':magnitude, 'people':num_people})
+    df.to_csv("data/data_"+name+".csv")
 
 
    
